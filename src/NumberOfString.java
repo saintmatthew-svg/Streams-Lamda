245c655738ec604cac09e951f83e2a169ec1c331
Onyekachi Matthew <onyekachimatthew00@gmail.com>
2025-08-04 11:13:32 +0100

    lamda/streams

--- src/NumberOfString.java
+++ src/NumberOfString.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class NumberOfString {
     public static void main(String[] args) {
+        String[] words = {"apple","banana","orange","umbrella"};
+        System.out.print(stringVowel(words));
+    }
 
+    public static int stringVowel(String[] words) {
+        return (int) Arrays.stream(words)
+                .filter(word -> "aeiou".indexOf(word.charAt(0)) != -1)
+                .count();
     }
 }
